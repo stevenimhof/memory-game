0a1af52fb06bff3fb271f8f0f9f5968b3d0c8855
Steven Imhof <steven.imhof@haufe.com>
2017-10-10 17:44:12 +0200

    working player vs player mode

--- src/components/game/game.ts
+++ src/components/game/game.ts
@@ -12,9 +12,11 @@ import { ComputerPlayer } from './player/computerPlayer';
 export class Game {
   private board;
   private currentOpenCards = [];
+  private cardStack = [];
   private leftCards = 12; // needs to be dynamic - change after adding settings
   private players = [];
-  private currentPlayerIndex = 1;
+  private currentPlayerIndex = 0;
+  private boardOverlay = false;
 
   constructor() {
     this.board = new Board();
@@ -26,19 +28,92 @@ export class Game {
     return this.board.getBoard();
   }
 
+  private play() {
+    this.getCurrentPlayer().makeMove();
+  }
+
   public flipEvent($event) {
+    // busy wait - don't accept flip events during another flip event
+    while(this.getBoardOverlay() === true) {}
+    this.setBoardOverlay(true);
+
+    // push card to stack
+    this.cardStack.push($event);
+
+    // we have 2 open cards
+    if (this.cardStack.length === 2) {
+
+      if (this.cardStack[0].name === this.cardStack[1].name) {
+        // we have a match
+        this.leftCards -= 2;
+        this.increaseScoreOfCurrentPlayer();
+
+        if (this.leftCards === 0) {
+          this.announceResult();
+        }
+
+        this.cardStack = [];
+        this.setBoardOverlay(false);
+      } else {
+        // pair does not match
+        Observable.timer(1000).subscribe(i => {
+          this.cardStack[0].isFlipped = false;
+          this.cardStack[1].isFlipped = false;
+          this.cardStack = [];
+          this.changeCurrentPlayer();
+          this.setBoardOverlay(false);
+
+          //is com turn?
+          /*console.log(this.getCurrentPlayer());
+          if (this.getCurrentPlayer() instanceof ComputerPlayer) {
+            this.setBoardOverlay(true);
+
+            var index = this.getCurrentPlayer().getMove();
+            var card = this.getCardByIndex(index);
+            Observable.timer(1000).subscribe(i => {
+              this.flipCard(card);
+
+              index = this.getCurrentPlayer().getMove();
+              card = this.getCardByIndex(index);
+
+              Observable.timer(1000).subscribe(i => {
+                this.flipCard(card);
+                this.setBoardOverlay(false);
+              });
+            });
+          }*/
+
+        });
+      }
+
+    } else {
+      this.setBoardOverlay(false);
+    }
+  }
+
+  /*public flipEvent___old($event) {
+
+    // busy wait
+    while(this.getBoardOverlay() === true) {}
+
+    this.setBoardOverlay(true);
+
     this.currentOpenCards.push($event);
-    console.log(this.currentOpenCards);
+    console.log(this.currentOpenCards.length);
     if (this.currentOpenCards.length === 2) {
 
       // if current player is com then make a random move
-      /*if (this.getCurrentPlayer() instanceof ComputerPlayer) {
-        let cardIndex = this.getCurrentPlayer().getMove();
+      if (this.getCurrentPlayer() instanceof ComputerPlayer) {
+        let index = this.getCurrentPlayer().getMove();
+        let card = this.getCardByIndex(index);
+        console.log(card);
         Observable.timer(1000).subscribe(i => {
-          this.currentOpenCards.push(cardIndex);
-          this.flipCard(cardIndex);
+          this.currentOpenCards.push(card);
+          console.log(this.currentOpenCards);
+          this.flipCard(card);
+          this.setBoardOverlay(false);
         });
-      }*/
+      }
 
       if (this.currentOpenCards[0].name === this.currentOpenCards[1].name) {
         // we have a match
@@ -57,10 +132,15 @@ export class Game {
           this.currentOpenCards[1].isFlipped = false;
           this.currentOpenCards = [];
           this.changeCurrentPlayer();
+          this.setBoardOverlay(false);
         });
       }
+    } else {
+      this.setBoardOverlay(false);
     }
-  }
+
+    console.log(this.getBoardOverlay());
+  }*/
 
   private announceResult() {
     if (this.hasWinner()) {
@@ -98,16 +178,28 @@ export class Game {
     return winner;
   }
 
-  private flipCard(index) {
+  private getCardByIndex(index) {
     let currentIndex = 0;
     for(let i = 0; i < 3; i++) {
       for (let j = 0; j < 4; j++) {
         currentIndex++;
         if (currentIndex === index) {
-          this.getBoard()[i][j].isFlipped = true;
+          return this.getBoard()[i][j];
         }
       }
     }
+  }
+
+  private flipCard(card) {
+    card.isFlipped = true;
+  }
+
+  private getBoardOverlay() {
+    return this.boardOverlay;
+  }
+
+  private setBoardOverlay(flag) {
+    this.boardOverlay = flag;
   }
 
 
